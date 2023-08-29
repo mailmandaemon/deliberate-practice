2ef20b103ed41938fd787e47fceca9294dcf43d4
mailmandaemon <mailman.industries@gmail.com>
2023-08-29 19:39:12 +1000

    course files and initial upload

---
+++ javascript/complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* class="between""btn again""number""left""guess""btn check""right""message"
+"label-score""label-highscore""highscore"*/
+/*span class="score"*/
+/* select document.querySelector('.between') */
+document.querySelector('.message').textContent = 'Correct Number';
+/* jspath document.querySelector("body > main > section.right > p.message")*/
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
